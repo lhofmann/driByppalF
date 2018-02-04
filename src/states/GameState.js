188670cd215f0644d66cdb76ef2e2a18f9168d3a
lhofmann <lhofmann@users.noreply.github.com>
2018-02-04 22:09:28 +0100

    Add flappy bird assets

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,12 +1,8 @@
-import RainbowText from 'objects/RainbowText';
-
 class GameState extends Phaser.State {
 
-	create() {
-		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "Hello, phaser!");
-		text.anchor.set(0.5);
-	}
+    create() {
+        
+    }
 
 }
 
---
+++ src/states/Menu.js
@@ -0,0 +1,34 @@
+class Menu extends Phaser.State {
+
+    create() {
+        let background = this.game.add.tileSprite(0, 0, 288, 512, 'background-day');
+        background.autoScroll(-200, 0);
+
+        let grass = this.game.add.tileSprite(0, 512 - 112, 288, 112, 'base');
+        grass.autoScroll(-200, 0);
+
+        this.message = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, 'message');
+        this.message.anchor.setTo(0.5, 0.5);
+
+        let tween = this.game.add.tween(this.message).to({
+            y: this.message.y + 20
+        }, 500, Phaser.Easing.Sinusoidal.InOut).to({
+            y: this.message.y
+        }, 500, Phaser.Easing.Sinusoidal.InOut, false, 0);
+        tween.loop(true);
+        tween.start();
+
+        this.game.input.onTap.add(this.onTap, this);        
+    }
+
+    onTap() {
+        this.state.start('GameState', false);
+    }
+
+    shutdown() {
+        this.message.kill();
+    }
+
+}
+
+export default Menu;

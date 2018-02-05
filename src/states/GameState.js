bb1452eb72224c2908d571ad280c7fdc94860692
lhofmann <lhofmann@users.noreply.github.com>
2018-02-05 21:35:02 +0100

    Standard flappy bird game play

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,7 +1,131 @@
 class GameState extends Phaser.State {
 
     create() {
-        
+        this.background = this.game.add.tileSprite(0, 0, 288, 512, 'background-day');
+        this.background.autoScroll(-100, 0);
+
+        this.grass = this.game.add.tileSprite(0, 512 - 112, 288, 112, 'base');
+        this.grass.autoScroll(-200, 0);        
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.bird = this.game.add.sprite(100, 245, 'bird');
+        this.bird.anchor.setTo(-0.2, 0.5);
+        this.game.physics.arcade.enable(this.bird);
+        this.bird.body.setCircle(Math.ceil(this.bird.height / 2));
+        this.bird.body.gravity.y = 1000;
+        this.bird.body.bounce.setTo(0.5, 0.5);
+        this.bird.alive = true;
+        this.bird.angle_min = -10;
+        this.bird.angle_max = 10;
+
+        this.pipes = this.game.add.group(); 
+
+        this.game.input.onTap.add(this.onTap, this);
+
+        this.pipe_width = this.game.cache.getImage('pipe').width;
+        this.pipe_height = this.game.cache.getImage('pipe').height;
+        this.last_pipe = null;
+
+        this.score = 0;
+
+        // this.createFlapTimer();
+
+        // delay appearance of first pipes
+        this.game.time.events.add(this.game.rnd.integerInRange(1000, 2000), this.addRowOfPipes, this); 
+    }
+
+    onHit() {
+        if (!this.bird.alive)
+            return;
+
+        this.bird.alive = false;
+        this.background.autoScroll(0, 0);
+        this.grass.autoScroll(0, 0);
+        this.pipes.forEach(function(pipe){
+            pipe.body.velocity.x = 0;
+        }, this);
+        this.game.time.events.remove(this.timer);
+    }
+
+    createFlapTimer() {
+        this.timer = this.game.time.events.add(this.game.rnd.integerInRange(250, 1000), this.autoFlap, this); 
+    }
+
+    autoFlap() {        
+        this.flap();
+        this.createFlapTimer();
+    }
+
+    addPipe(x, y, top) {
+        var pipe = this.game.add.sprite(x, y - (top ? 100 : 0), 'pipe');
+        if (top)
+            pipe.scale.y *= -1;
+
+        this.pipes.add(pipe);
+        this.game.physics.arcade.enable(pipe);
+        pipe.body.velocity.x = -200;
+        pipe.body.immovable = true;
+        pipe.count_score = top;
+        this.last_pipe = pipe;
+    }
+
+    addRowOfPipes() {
+        let pipe_offset = this.game.rnd.integerInRange(150, this.game.cache.getImage('pipe').height);
+        let hole = this.game.world.height - pipe_offset;
+        this.addPipe(this.game.world.width, hole, false);
+        this.addPipe(this.game.world.width, hole, true);
+        this.game.world.bringToTop(this.grass);
+    }
+
+    update() {    
+        if (this.bird.alive)
+            this.game.physics.arcade.overlap(this.bird, this.pipes, this.onHit, null, this);
+        else
+            this.game.physics.arcade.collide(this.bird, this.pipes);
+
+        if (this.last_pipe) {
+            this.pipes.forEachAlive((function(pipe) {
+                if (pipe.count_score && this.bird.x > this.last_pipe.x + this.pipe_width) {
+                    pipe.count_score = false;
+                    this.score += 1;
+                    console.log(this.score);
+                }
+                if (pipe.x < -this.pipe_width)
+                    pipe.kill();
+            }), this);
+
+            if (this.last_pipe.x < 15)
+                this.addRowOfPipes();
+        }
+
+        if (this.bird.alive) {
+            if (this.bird.angle < 20)
+                this.bird.angle += 1; 
+            if (this.bird.y > this.game.world.height - 150)
+                this.flap();
+        } else {            
+            if (this.bird.bottom > this.grass.y) {
+                this.bird.bottom = this.grass.y;
+                this.bird.body.velocity.y = 0;
+            }
+        }
+    }
+
+    flap() {
+        if (!this.bird.alive)
+            return;
+
+        if (this.bird.y > 75) {
+            this.bird.body.velocity.y = -350;
+            this.game.add.tween(this.bird).to({angle: -20}, 100).start();
+        }
+    }
+
+    onTap() {
+        if (this.bird.alive)
+            this.flap();
+        else
+            this.game.state.restart();
     }
 
 }
--- src/states/Menu.js
+++ src/states/Menu.js
@@ -2,18 +2,19 @@ class Menu extends Phaser.State {
 
     create() {
         let background = this.game.add.tileSprite(0, 0, 288, 512, 'background-day');
-        background.autoScroll(-200, 0);
+        background.autoScroll(-100, 0);
 
         let grass = this.game.add.tileSprite(0, 512 - 112, 288, 112, 'base');
         grass.autoScroll(-200, 0);
+        this.game.world.bringToTop(grass);
 
-        this.message = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, 'message');
-        this.message.anchor.setTo(0.5, 0.5);
+        let message = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, 'message');
+        message.anchor.setTo(0.5, 0.5);
 
-        let tween = this.game.add.tween(this.message).to({
-            y: this.message.y + 20
+        let tween = this.game.add.tween(message).to({
+            y: message.y + 20
         }, 500, Phaser.Easing.Sinusoidal.InOut).to({
-            y: this.message.y
+            y: message.y
         }, 500, Phaser.Easing.Sinusoidal.InOut, false, 0);
         tween.loop(true);
         tween.start();
@@ -22,11 +23,7 @@ class Menu extends Phaser.State {
     }
 
     onTap() {
-        this.state.start('GameState', false);
-    }
-
-    shutdown() {
-        this.message.kill();
+        this.state.start('GameState');
     }
 
 }
